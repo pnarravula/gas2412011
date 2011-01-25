f16e56cd16411e5d78ffa6f75f29f6f005ef7d44
prasad narravula <pnarravulla@invisioninc.com>
2011-01-25 13:44:15 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
@@ -1,18 +1,28 @@
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace nothinbutdotnetstore.web.core
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+        IEnumerable<RequestCommand> all_commands;
+
         public DefaultCommandRegistry(IEnumerable<RequestCommand> all_commands)
         {
-            
+            this.all_commands = all_commands;
         }
 
         public RequestCommand get_command_that_can_run(Request request)
         {
-            throw new NotImplementedException();
+            //var iterator =  all_commands.GetEnumerator();
+            //RequestCommand request_command = null;
+            //while (iterator.MoveNext())
+            //{
+            //    request_command = iterator.Current;
+            //}
+            //return request_command;
+
+            return all_commands.Last();
         }
     }
 }
